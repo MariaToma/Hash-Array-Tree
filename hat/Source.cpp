e2ead4d5278fbf3d61d9a2880098399fc7889ab9
MariaToma <mariaaaa0833@yahoo.com>
2016-06-07 13:12:42 +0300

    new

--- hat/Source.cpp
+++ hat/Source.cpp
@@ -21,7 +21,7 @@ BestStructEver mutari[M];
 int dim = 0;
 
 const int offsetX = 30;
-const int offsetY = 100;
+const int offsetY = 100; //dimensiunile "memoriei" reprezentata prin matrice
 
 const wchar_t g_szClassName[] = L"myWindowClass";
 int matrice[M];
@@ -57,4 +57,39 @@ void increment(HWND hwnd) {
 		MessageBox(hwnd, L"Out of memory!!!", L"ERROR", MB_ICONERROR); //afisez mesajul
 		SendMessage(hwnd, WM_DESTROY, NULL, NULL); // inchid programul ?? 
 	}
+}
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow) {
+	WNDCLASSEX wc;
+	HWND hwnd;
+	MSG Msg;
+	// Initializari ale matricei:
+	srand(time(NULL));
+	init();
+	//Step 1: Registering the Window Class
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = 0;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = g_szClassName;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wc)) {
+		MessageBox(NULL, L"Window Registration Failed!", L"Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	// Step 3: The Message Loop
+	while (GetMessage(&Msg, NULL, 0, 0) > 0) {
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+
 }
